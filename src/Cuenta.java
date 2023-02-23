673981bde9cbbd0e62980be83392f4c8acf3b67d
JeanAranda <jarandar2014@gmail.com>
2023-02-23 00:55:27 -0500

    Se avanzo prueba de metodos

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -3,4 +3,22 @@ class Cuenta {
 		int agencia;
 		int numero;
 		String titular;
+		
+		//No retorna valor
+		public void depositar(double valor) {
+			this.saldo = this.saldo + valor;
+ 		}
+
+		//Retorna valor
+		public boolean retirar(double valor) {
+			if  (this.saldo >= valor ) {
+				this.saldo = this.saldo - valor;
+				return true;
+				}else{
+					return false;
+				}
+		}
+		
 }
+			
+
