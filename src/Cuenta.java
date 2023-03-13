071b775bb70a20a0ee998edcc5bc7546ebd21286
JeanAranda <jarandar2014@gmail.com>
2023-03-12 23:52:03 -0500

    se avanzo hasta intentar cambiar depositar de void a boolea

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -4,21 +4,36 @@ class Cuenta {
 		int numero;
 		String titular;
 		
-		//No retorna valor
-		public void depositar(double valor) {
-			this.saldo = this.saldo + valor;
- 		}
-
-		//Retorna valor
+		// No retorna valor
+		public void depositar(double saldo) {
+		this.saldo = this.saldo + saldo;
+		}
+		
 		public boolean retirar(double valor) {
-			if  (this.saldo >= valor ) {
-				this.saldo = this.saldo - valor;
-				return true;
-				}else{
+				if  (this.saldo >= valor) {
+					this.saldo = this.saldo -valor;
+					
+					System.out.println("Retiro exitoso!!!");
+					System.out.println(valor);
+					return true;
+				}else {
 					return false;
 				}
 		}
+		public boolean trasnferir(double valor, Cuenta cuenta) {
+			if (this.saldo >= valor) {
+				
+				this.saldo = this.saldo - valor;
+				cuenta.depositar(valor);
+				System.out.println("Transferencia exitosa!!");
+				System.out.println("Se envio "+ valor + " a la cuenta de "+ cuenta.titular);
+				return true;
+			}else {
+				return false;
+			}
+		}
 		
-}
+		
+		}
 			
 
--- src/PruebaMetodos.java
+++ src/PruebaMetodos.java
@@ -1,13 +1,35 @@
+import java.lang.reflect.AnnotatedArrayType;
 
 public class PruebaMetodos {
 	public static void main(String[] args) {
-		Cuenta miCuenta = new Cuenta();
-		miCuenta.saldo = 300;
-		miCuenta.depositar(200);
+		Cuenta jpCuenta = new Cuenta();
+		jpCuenta.titular = "Jean Pierre Aranda"; 
+		//saldo actual
+		jpCuenta.saldo = 1000;
+		System.out.println("Bienvenido "+jpCuenta.titular+" su cuenta fue aperturada con exito y su saldo de apertura es:");
+		System.out.println(jpCuenta.saldo);
+		//deposito
+		jpCuenta.depositar(10300);
+		System.out.println(jpCuenta.titular+" Depositaste a tu cuenta"
+				);
+		System.out.println(jpCuenta.titular+" su saldo actual es: "+jpCuenta.saldo);
+		//retiro
+		System.out.println(jpCuenta.titular+" Retiraste de tu cuenta:");
+		jpCuenta.retirar(500);
+		//saldo actual		
+		System.out.println("Estimado "+ jpCuenta.titular + " su actual es: "+jpCuenta.saldo);
+		Cuenta annionCuenta = new Cuenta();
+		annionCuenta.titular = "Annie Begazo Beltran";
+		annionCuenta.saldo = 5000;
+		System.out.println("Bienvenida "+annionCuenta.titular+
+				" su cuenta fue aperturada con exito y su saldo de apertura es:"+annionCuenta.saldo);
+		annionCuenta.depositar(2000);
+		System.out.println(annionCuenta.titular+ " Depostaste a tu cuenta");
+
+		System.out.println("Estimada "+ annionCuenta.titular + " su saldo es: "+ annionCuenta.saldo);
+		annionCuenta.trasnferir(1500, jpCuenta);
+		System.out.println("El saldo de: "+annionCuenta.titular+" es "+annionCuenta.saldo);
 		
-		System.out.println(miCuenta.saldo);
-		
-		miCuenta.retirar(100);
-		System.out.println(miCuenta.saldo);
+		System.out.println("Saldo de "+jpCuenta.titular+" es: "+jpCuenta.saldo);
 	}
 }
